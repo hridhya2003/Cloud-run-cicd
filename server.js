f270c46483ee9f62f6b72f306631d9740d9eabc5
hridhya2003 <hridhyaaugustine11@gmail.com>
2025-02-21 08:49:39 +0000

    Initial commit: Add Node.js app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 970;
 
 app.get('/', (req, res) => {
     res.send('Helloooo, Cloud Run!');
