a900971903266d358c7e43293d35d30ed7c94a99
hridhya2003 <hridhyaaugustine11@gmail.com>
2025-02-22 08:23:17 +0000

    Added server.js and Cloud-run-cicd directory

---
+++ sever.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; // Change the port to 980
+
+app.get('/', (req, res) => {
+    res.send('Hello, Cloud Run!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
