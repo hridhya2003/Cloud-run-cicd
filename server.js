fd670ea2a3b7dec1d98a6186d98eab4effb56319
hridhya2003 <hridhyaaugustine11@gmail.com>
2025-02-22 09:15:51 +0000

    added

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Set the port from the environment variable or use 8080 by default
-const PORT = process.env.PORT || 3000;
-
-// Define a basic route
-app.get("/", (req, res) => {
-  res.send("Hello, Cloud Run! 🚀 Your Node.js server is running.");
+app.get('/', (req, res) => {
+    res.send('Hello, GCP! Your Node.js app is running.');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
